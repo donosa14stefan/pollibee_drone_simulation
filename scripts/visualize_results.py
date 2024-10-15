26ff0d7e6c2bf1cabbb82af358381470a50a5e59
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-15 18:32:29 +0300

    Update visualize_results.py

--- scripts/visualize_results.py
+++ scripts/visualize_results.py
@@ -1,39 +1,48 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
-import numpy as np
-import matplotlib.pyplot as plt
+import cv2
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 
 class Visualizer:
     def __init__(self):
         self.node_name = "visualizer"
+        rospy.init_node(self.node_name)
         self.rate = rospy.Rate(10)  # 10 Hz
 
-        # Subscribers
-        self.image_sub = rospy.Subscriber("image_with_detections", Image, self.image_callback)
-
-        # Create CvBridge object
+        # Initialize CV Bridge
         self.bridge = CvBridge()
 
-    def image_callback(self, image_msg):
-        # Convert ROS image message to OpenCV image
-        cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+        # Subscribers
+        self.image_sub = rospy.Subscriber("yolo_detections", Image, self.image_callback)
 
-        # Display image with detections
-        plt.imshow(cv_image)
-        plt.axis("off")
-        plt.show(block=False)
-        plt.pause(0.01)
+        # OpenCV window
+        cv2.namedWindow("Pollibee Drone Simulation", cv2.WINDOW_NORMAL)
+
+    def image_callback(self, image_msg):
+        try:
+            # Convert ROS Image message to OpenCV image
+            cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+
+            # Display image
+            cv2.imshow("Pollibee Drone Simulation", cv_image)
+            cv2.waitKey(1)
+
+        except CvBridgeError as e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
 
     def run(self):
-        rospy.init_node(self.node_name)
         rospy.loginfo("Visualizer started")
-
         while not rospy.is_shutdown():
             self.rate.sleep()
 
+    def __del__(self):
+        cv2.destroyAllWindows()
+
 if __name__ == "__main__":
-    visualizer = Visualizer()
-    visualizer.run()
+    try:
+        visualizer = Visualizer()
+        visualizer.run()
+    except rospy.ROSInterruptException:
+        pass
