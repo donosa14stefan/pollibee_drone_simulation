7ce9d38db7547d5d1596cadf1b402b960bfde738
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-18 21:58:00 +0300

    Update yolo_plugin.cpp

--- plugins/yolo/yolo_plugin.cpp
+++ plugins/yolo/yolo_plugin.cpp
@@ -1,47 +1,32 @@
+// File: plugins/yolo/yolo_plugin.cpp
+
 #include "yolo_plugin.h"
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
 
-namespace yolo {
-class YoloPlugin : public ros::Node {
-public:
-  YoloPlugin() : ros::Node("yolo_plugin") {
-    // Initialize ROS node
-    this->image_transport_ = new image_transport::ImageTransport(this->node_handle_);
-    this->image_pub_ = this->image_transport_->advertise("yolo_image", 1);
-    this->camera_sub_ = this->node_handle_.subscribe("camera_image", 1, &YoloPlugin::cameraCallback, this);
+namespace yolo
+{
+  YoloPlugin::YoloPlugin() : ros::Node("yolo_plugin")
+  {
+    this->imageTransport = new image_transport::ImageTransport(this->nodeHandle);
+    this->imagePub = this->imageTransport->advertise("yolo_image", 1);
+    this->cameraSub = this->nodeHandle.subscribe("camera_image", 1, &YoloPlugin::cameraCallback, this);
   }
 
-  ~YoloPlugin() {
-    // Clean up
-    delete this->image_transport_;
+  YoloPlugin::~YoloPlugin()
+  {
+    delete this->imageTransport;
   }
 
-  void cameraCallback(const sensor_msgs::Image::ConstPtr& msg) {
-    // Convert image message to OpenCV Mat
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-
-    // Run YOLO object detection on the image
-    cv::Mat detected_image = runYolo(cv_ptr->image);
-
-    // Publish detected image
-    sensor_msgs::Image::Ptr detected_image_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", detected_image).toImageMsg();
-    this->image_pub_.publish(detected_image_msg);
+  void YoloPlugin::cameraCallback(const sensor_msgs::Image::ConstPtr &_msg)
+  {
+    cv_bridge::CvImagePtr cvPtr = cv_bridge::toCvCopy(_msg, sensor_msgs::image_encodings::BGR8);
+    cv::Mat detectedImage = runYolo(cvPtr->image);
+    sensor_msgs::Image::Ptr detectedImageMsg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", detectedImage).toImageMsg();
+    this->imagePub.publish(detectedImageMsg);
   }
 
-private:
-  ros::NodeHandle node_handle_;
-  image_transport::ImageTransport* image_transport_;
-  image_transport::Publisher image_pub_;
-  ros::Subscriber camera_sub_;
-
-  cv::Mat runYolo(const cv::Mat& image) {
-    // TO DO: implement YOLO object detection algorithm
-    return image; // placeholder
+  cv::Mat YoloPlugin::runYolo(const cv::Mat &_image)
+  {
+    // Implementează algoritmul YOLO pentru detectarea obiectelor
+    return _image; // placeholder
   }
-};
-
-PLUGINLIB_EXPORT_CLASS(yolo::YoloPlugin, ros::Node)
 }
