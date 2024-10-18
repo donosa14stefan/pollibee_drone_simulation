6989cfb6e4ccda2e76b9c25969ffb6823aff8233
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-18 21:56:20 +0300

    Update ros_plugin.cpp

--- plugins/ros/ros_plugin.cpp
+++ plugins/ros/ros_plugin.cpp
@@ -1,32 +1,27 @@
+// File: plugins/ros/ros_plugin.cpp
+
 #include "ros_plugin.h"
-#include <ros/ros.h>
-#include <ros/node_handle.h>
-#include <ros/publisher.h>
-#include <ros/subscriber.h>
-#include <sensor_msgs/Image.h>
 
-namespace ros {
-class RosPlugin : public ros::Node {
-public:
-  RosPlugin() : ros::Node("ros_plugin") {
-    // Initialize ROS publishers and subscribers
-    this->image_pub_ = this->node_handle_.advertise<sensor_msgs::Image>("image", 10);
-    this->camera_sub_ = this->node_handle_.subscribe("camera", 10, &RosPlugin::cameraCallback, this);
+namespace ros
+{
+  RosPlugin::RosPlugin() : ros::Node("ros_plugin")
+  {
+    this->nodeHandle = ros::NodeHandle("~");
+    this->imagePub = this->nodeHandle.advertise<sensor_msgs::Image>("image", 10);
+    this->cameraSub = this->nodeHandle.subscribe("camera", 10, &RosPlugin::cameraCallback, this);
   }
 
-  void cameraCallback(const sensor_msgs::Image::ConstPtr& msg) {
-    // Process camera image message
+  RosPlugin::~RosPlugin()
+  {
   }
 
-  void publishImage(const sensor_msgs::Image& image) {
-    this->image_pub_.publish(image);
+  void RosPlugin::cameraCallback(const sensor_msgs::Image::ConstPtr &_msg)
+  {
+    // Procesează imaginea de la camera
   }
 
-private:
-  ros::NodeHandle node_handle_;
-  ros::Publisher image_pub_;
-  ros::Subscriber camera_sub_;
-};
-
-PLUGINLIB_EXPORT_CLASS(ros::RosPlugin, ros::Node)
+  void RosPlugin::publishImage(const sensor_msgs::Image &_msg)
+  {
+    this->imagePub.publish(_msg);
+  }
 }
