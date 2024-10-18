3400e6ca2ee48313e91df49c5389843852c7cfdf
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-18 21:55:24 +0300

    Update python_plugin.cpp

--- plugins/python/python_plugin.cpp
+++ plugins/python/python_plugin.cpp
@@ -1,42 +1,32 @@
+// File: plugins/python/python_plugin.cpp
+
 #include "python_plugin.h"
-#include <ros/ros.h>
 #include <ros/python_init.h>
 #include <ros/names.h>
 #include <boost/python.hpp>
 
-namespace python {
-class PythonPlugin : public ros::Node {
-public:
-  PythonPlugin() : ros::Node("python_plugin") {
-    // Initialize ROS node
-    this->node_handle_ = ros::NodeHandle("~");
-    this->python_thread_ = boost::thread(boost::bind(&PythonPlugin::runPython, this));
+namespace python
+{
+  PythonPlugin::PythonPlugin() : ros::Node("python_plugin")
+  {
+    this->nodeHandle = ros::NodeHandle("~");
+    this->pythonThread = boost::thread(boost::bind(&PythonPlugin::runPython, this));
   }
 
-  ~PythonPlugin() {
-    // Clean up
-    this->python_thread_.join();
+  PythonPlugin::~PythonPlugin()
+  {
+    this->pythonThread.join();
   }
 
-private:
-  ros::NodeHandle node_handle_;
-  boost::thread python_thread_;
-
-  void runPython() {
-    // Initialize Python interpreter
+  void PythonPlugin::runPython()
+  {
     Py_Initialize();
     PyEval_InitThreads();
 
-    // Import Python module
     boost::python::object module = boost::python::import("python_module");
 
-    // Run Python code
     module.attr("run")();
 
-    // Clean up
     Py_Finalize();
   }
-};
-
-PLUGINLIB_EXPORT_CLASS(python::PythonPlugin, ros::Node)
 }
