8aadd62870ee797338a15a1ca34d8a4b0c5a0c2c
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-19 15:30:14 +0300

    Update battery_plugin.h

--- plugins/gazebo/battery_plugin/battery_plugin.h
+++ plugins/gazebo/battery_plugin/battery_plugin.h
@@ -3,6 +3,7 @@
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
 #include <geometry_msgs/Twist.h>
@@ -14,12 +15,19 @@ namespace gazebo
     public:
       BatteryPlugin();
       virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
-      virtual void OnUpdate(const common::UpdateInfo & /*_info*/);
 
     private:
+      void OnUpdate(const common::UpdateInfo &_info);
+      void VelocityCallback(const geometry_msgs::Twist::ConstPtr& msg);
+      void UpdateBatteryStatus();
+      void PublishBatteryStatus();
+      void CalculateDischargeRate(const geometry_msgs::Twist& velocity);
+
       physics::ModelPtr model;
+      physics::WorldPtr world;
       event::ConnectionPtr updateConnection;
-      ros::NodeHandle* rosNode;
+      
+      std::unique_ptr<ros::NodeHandle> rosNode;
       ros::Publisher batteryPub;
       ros::Subscriber velocitySub;
       
@@ -29,14 +37,8 @@ namespace gazebo
       double chargingRate;
       double lowBatteryThreshold;
       bool isCharging;
-      
-      void UpdateBatteryStatus();
-      void VelocityCallback(const geometry_msgs::Twist::ConstPtr& msg);
-      void CalculateDischargeRate(const geometry_msgs::Twist& velocity);
-      void PublishBatteryStatus();
-      bool NeedsCharging() const;
-      void StartCharging();
-      void StopCharging();
+
+      common::Time lastUpdateTime;
   };
 }
 #endif
