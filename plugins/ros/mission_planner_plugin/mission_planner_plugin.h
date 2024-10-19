90035f89e1614845c92c3b64b87112ec9d584e67
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-19 15:31:24 +0300

    Update mission_planner_plugin.h

--- plugins/ros/mission_planner_plugin/mission_planner_plugin.h
+++ plugins/ros/mission_planner_plugin/mission_planner_plugin.h
@@ -1,47 +1,49 @@
 #ifndef MISSION_PLANNER_PLUGIN_HH
 #define MISSION_PLANNER_PLUGIN_HH
 
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
 #include <ros/ros.h>
-#include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Path.h>
 #include <std_msgs/Float32.h>
-#include <std_msgs/Bool.h>
 #include <vector>
 
-class MissionPlannerPlugin
+namespace gazebo
 {
-  public:
-    MissionPlannerPlugin();
-    void initialize(ros::NodeHandle& nh);
-    void planMission();
-    void updateMission();
+  class MissionPlannerPlugin : public ModelPlugin
+  {
+    public:
+      MissionPlannerPlugin();
+      virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
-  private:
-    ros::NodeHandle* nh_;
-    ros::Publisher pathPub_;
-    ros::Subscriber poseSub_;
-    ros::Subscriber flowerDetectionSub_;
-    ros::Subscriber batterySub_;
-    ros::Subscriber windSpeedSub_;
-    ros::Subscriber windDirectionSub_;
-    
-    nav_msgs::Path currentPath_;
-    geometry_msgs::PoseStamped currentPose_;
-    std::vector<geometry_msgs::PoseStamped> flowerPositions_;
-    std::vector<geometry_msgs::PoseStamped> surveyPoints_;
-    double windSpeed_;
-    ignition::math::Vector3d windDirection_;
-    double batteryLevel_;
-    bool isPollinating_;
-    
-    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
-    void flowerDetectionCallback(const std_msgs::Bool::ConstPtr& msg);
-    void batteryCallback(const std_msgs::Float32::ConstPtr& msg);
-    void windSpeedCallback(const std_msgs::Float32::ConstPtr& msg);
-    void windDirectionCallback(const geometry_msgs::Vector3::ConstPtr& msg);
-    void generateSurveyPoints();
-    void generatePath();
-    void updatePath();
-    void publishPath();
-};
+    private:
+      void OnUpdate(const common::UpdateInfo &_info);
+      void GenerateSurveyPath();
+      void UpdateMission();
+      void PublishCurrentGoal();
+      void BatteryCallback(const std_msgs::Float32::ConstPtr& msg);
+      void PoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+      physics::ModelPtr model;
+      physics::WorldPtr world;
+      event::ConnectionPtr updateConnection;
+      
+      std::unique_ptr<ros::NodeHandle> rosNode;
+      ros::Publisher goalPub;
+      ros::Publisher pathPub;
+      ros::Subscriber batterySub;
+      ros::Subscriber poseSub;
+      
+      std::vector<ignition::math::Vector3d> waypoints;
+      size_t currentWaypointIndex;
+      double waypointReachedThreshold;
+      double batteryLevel;
+      ignition::math::Vector3d currentPosition;
+      bool missionCompleted;
+
+      common::Time lastUpdateTime;
+  };
+}
 #endif
