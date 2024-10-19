0b5551c5a927664c1f71edae1077417ea17876c8
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-19 18:34:06 +0300

    Update battery_plugin.cpp

--- plugins/gazebo/battery_plugin/battery_plugin.cpp
+++ plugins/gazebo/battery_plugin/battery_plugin.cpp
@@ -1,10 +1,12 @@
 #include "battery_plugin.h"
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/Link.hh>
 
 namespace gazebo
 {
   GZ_REGISTER_MODEL_PLUGIN(BatteryPlugin)
 
-  BatteryPlugin::BatteryPlugin() : batteryCapacity(100), currentCharge(100), 
+  BatteryPlugin::BatteryPlugin() : batteryCapacity(100), currentCharge(100),
     dischargeRate(0.1), chargingRate(1.0), lowBatteryThreshold(20), isCharging(false) {}
 
   void BatteryPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
@@ -27,39 +29,39 @@ namespace gazebo
     this->velocitySub = this->rosNode->subscribe("cmd_vel", 1000, &BatteryPlugin::VelocityCallback, this);
 
     this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-        std::bind(&BatteryPlugin::OnUpdate, this, std::placeholders::_1));
+        std::bind(&BatteryPlugin::OnUpdate, this));
 
     this->lastUpdateTime = this->world->SimTime();
 
     ROS_INFO("Battery plugin initialized");
   }
 
-  void BatteryPlugin::OnUpdate(const common::UpdateInfo &_info)
-  {
-    this->UpdateBatteryStatus();
-    this->PublishBatteryStatus();
-  }
-
-  void BatteryPlugin::UpdateBatteryStatus()
+  void BatteryPlugin::OnUpdate()
   {
     common::Time currentTime = this->world->SimTime();
     double dt = (currentTime - this->lastUpdateTime).Double();
 
+    this->UpdateBatteryStatus(dt);
+    this->PublishBatteryStatus();
+
+    this->lastUpdateTime = currentTime;
+  }
+
+  void BatteryPlugin::UpdateBatteryStatus(double dt)
+  {
     if (this->isCharging)
     {
       this->currentCharge = std::min(this->batteryCapacity, this->currentCharge + this->chargingRate * dt);
     }
     else
     {
-            this->currentCharge = std::max(0.0, this->currentCharge - this->dischargeRate * dt);
+      this->currentCharge = std::max(0.0, this->currentCharge - this->dischargeRate * dt);
     }
 
     if (this->currentCharge <= this->lowBatteryThreshold && !this->isCharging)
     {
       ROS_WARN("Low battery! Current charge: %.2f%%", this->currentCharge);
     }
-
-    this->lastUpdateTime = currentTime;
   }
 
   void BatteryPlugin::VelocityCallback(const geometry_msgs::Twist::ConstPtr& msg)
@@ -76,18 +78,15 @@ namespace gazebo
 
   void BatteryPlugin::CalculateDischargeRate(const geometry_msgs::Twist& velocity)
   {
-    // Calculăm rata de descărcare bazată pe viteza curentă
-    // Cu cât viteza este mai mare, cu atât consumul de energie este mai mare
-    double linearVelocity = std::sqrt(velocity.linear.x * velocity.linear.x + 
-                                      velocity.linear.y * velocity.linear.y + 
+    double linearVelocity = std::sqrt(velocity.linear.x * velocity.linear.x +
+                                      velocity.linear.y * velocity.linear.y +
                                       velocity.linear.z * velocity.linear.z);
-    double angularVelocity = std::abs(velocity.angular.z);
+    double angularVelocity = std::abs(velocity .angular.z);
 
-    // Factori de consum pentru mișcarea liniară și rotațională
     double linearConsumptionFactor = 0.1;
     double angularConsumptionFactor = 0.05;
 
-    this->dischargeRate = 0.05 + linearVelocity * linearConsumptionFactor + 
+    this->dischargeRate = 0.05 + linearVelocity * linearConsumptionFactor +
                           angularVelocity * angularConsumptionFactor;
   }
 }
